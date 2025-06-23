cb356818d058bd1057a8bf0d3e310ac6161c647f
yonghwan0688 <yonghwan0688@gmail.com>
2025-06-23 16:55:58 +0900

    0623_퇴근

---
+++ dev/study/0623/main.js
@@ -0,0 +1,102 @@
+// alert("Hello World!");
+// console.log("Hello World!");
+// document.write("<h1>Document Write<h1>");
+
+// let currentYear = 2025;
+// let birthYear = prompt("태어난 연도를 입력하세요.")
+// let age = currentYear - birthYear + 1;
+// alert("당신의 나이는 " + age + "살입니다.");
+
+// let name = "철수";
+// let age = 20;
+// let isStudent = true;
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isStudent);
+
+// let price = 10000;
+// let rate = 0.2;
+// let discount = price * rate;
+// let finalPrice = price - discount;
+
+// console.log("할인된 가격은 " + finalPrice + "원 입니다.");
+
+// let name = "영희";
+// console.log(`안녕하세요, ${name}님!`);
+
+// let a = 10;
+// console.log(a);
+// a += 5;
+// console.log(a);
+// a-= 3;
+// console.log(a);
+// a *= 2;
+// console.log(a);
+// a /= 4;
+// console.log(a);
+// console.log(`최종 값은 ${a} 입니다.`);
+
+// 1. 이름과 나이를 입력받아 인사말 출력하기
+// 2. 두 수를 입력받아 사칙연산 결과를 각각 출력하기
+// 3. == VS === 비교 실험하기
+
+// let name = prompt("이름을 입력하세요");
+// let age = prompt("나이를 입력하세요.");
+// let height = prompt("키를 입력하세요.");
+// let weight = prompt("몸무게를 입력하세요.");
+
+// alert(`안녕하세요. ${name}님 반갑습니다. 당신의 현재 나이는 ${age}입니다. 맞으시면 다음 단계로 가십시오.`);
+// alert(`당신의 비만지수는 ${(weight / height) * height} 입니다.`);
+
+// 실습: 3의 배수 검사기
+// let num = prompt("숫자를 입력하세요.");
+// if (num % 3 === 0) {
+//     alert("3의 배수입니다.");
+// } else {
+//     alert("3의 배수가 아닙니다.");
+// } 
+
+// let day = prompt("오늘 요일을 입력하세요");
+// switch (day) {
+//     case "월요일":
+//         alert("한 주의 시작!");
+//         break;
+//     case "금요일":
+//         alert("불타는 금요일!");
+//         break;
+//     default:
+//         alert("평범한 하루네요.");
+// }   
+
+// let a = 5;
+// let b = 3;
+// alert(a + b);
+
+// let name = "홍길동";
+// let age = 20;
+// alert(`${name}님은 ${age}살입니다.`);
+
+// let side = 4;
+// alert(side*side);
+
+let number = 7;
+    if (number % 2 === 0) {
+        console.log("짝수입니다.");
+    } else {
+        console.log("홀수입니다.");
+        }
+
+let age = 16;
+    if(age < 19) {
+        console.log("미성년자입니다.");
+    } else{
+        console.log("성인입니다.");
+    }
+
+let password = "1234";
+let input = "1234"
+
+    if (password === input) alert("로그인 성공");
+    else alert("로그인 실패");
+

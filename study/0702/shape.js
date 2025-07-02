f547b2c26e9b334ae160107d0ed16a933c228b07
yonghwan0688 <yonghwan0688@gmail.com>
2025-07-02 11:17:13 +0900

    0702_update

---
+++ study/0702/shape.js
@@ -0,0 +1,8 @@
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+var jill = { name: "jill" };
+var person = jill;
+console.log(person);
---
+++ study/0702/shape.ts
@@ -0,0 +1,9 @@
+class Person {
+  name: string;
+}
+
+const jill: { name: string } = { name: "jill" };
+
+const person: Person = jill;
+
+console.log(person);

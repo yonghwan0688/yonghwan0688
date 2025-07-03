41f483f1e41a19fb09122be4f32177019d31e988
yonghwan0688 <yonghwan0688@gmail.com>
2025-07-03 18:53:08 +0900

    update8

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,23 +1,17 @@
-function solution(s){
- let counter = 0;
- for(let i = 0; i < s.length; i++){
-     if(s[i] === '('){
-         counter += 1;
-     } else {
-         counter -= 1;
-     }
-     
-    
-     
-     if(counter<0){ 
-         return false;
-     }
- }   
-    
-    if(counter !== 0) return false 
-   return true
-// if(s[0] === '(' && s[s.length-1] === ')'){
-//         return true;
-//     } else return false;
- 
+function solution(s) {
+  let counter = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      counter += 1;
+    } else {
+      counter -= 1;
+    }
+
+    if (counter < 0) {
+      return false;
+    }
+  }
+
+  if (counter !== 0) return false;
+  return true;
 }

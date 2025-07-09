983c36b624a56c14486849f727402eef7c0be912
yonghwan0688 <yonghwan0688@gmail.com>
2025-07-09 14:14:09 +0900

    0709_1

---
+++ study/0709/ch02_4/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import EventListener from "./pages/EventListener";
+import ReactOnClick from "./pages/ReactOnClick";
+import EventBubbling from "./pages/EventBubbling";
+import FileInput from "./pages/FileInput";
+
+export default function App() {
+  return (
+    <div>
+      {/* <EventListener /> */}
+      {/* <ReactOnClick /> */}
+      {/* <EventBubbling /> */}
+      <FileInput />
+    </div>
+  );
+}

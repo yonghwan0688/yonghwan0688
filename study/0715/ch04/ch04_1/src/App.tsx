2b065db162573fe18926d37d3aff5b8c9f9e538a
yonghwan0688 <yonghwan0688@gmail.com>
2025-07-15 16:13:08 +0900

    0715_7

--- study/0715/ch04/ch04_1/src/App.tsx
+++ study/0715/ch04/ch04_1/src/App.tsx
@@ -1,13 +1,46 @@
-import ButtonTest from './pages/ButtonTest'
-import InputTest from './pages/InputTest'
-import ModalTest from './pages/ModalTest'
+import './App.css'
+import Clock from './pages/Clock'
+import {useEffect, useState} from 'react'
+
+// export default function App() {
+//   const [today, setToday] = useState(new Date())
+
+//   useEffect(() => {
+//     const duration = 1000
+//     const id = setInterval(() => {
+//       setToday(new Date())
+//     }, duration)
+//     return () => clearInterval(id)
+//   }, [])
+
+//   return (
+//     <main>
+//       <Clock today={today} />
+//     </main>
+//   )
+// }
 
 export default function App() {
+  const [number1, setNumber1] = useState(0)
+  const [number2, setNumber2] = useState(0)
+
   return (
-    <main>
-      <ButtonTest />
-      <InputTest />
-      <ModalTest />
-    </main>
+    <div>
+      <h2>("덧셈기")</h2>
+      <div className="flex flex-row items-center">
+        <input
+          type="number"
+          value={number1}
+          onChange={e => setNumber1(parseInt(e.target.value))}
+        />
+        <span>{'+'}</span>
+        <input
+          type="number"
+          value={number2}
+          onChange={e => setNumber2(parseInt(e.target.value))}
+        />
+      </div>
+      <p>{`덧셈결과는 : ${number1} + ${number2} = ${number1 + number2}`}</p>
+    </div>
   )
 }

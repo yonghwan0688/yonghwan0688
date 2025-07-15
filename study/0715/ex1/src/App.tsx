7c16039767ca310b2e3abc354acce283ef944b7a
yonghwan0688 <yonghwan0688@gmail.com>
2025-07-15 12:44:26 +0900

    0715_5

---
+++ study/0715/ex1/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return <Card />;
+}
+
+export default App;

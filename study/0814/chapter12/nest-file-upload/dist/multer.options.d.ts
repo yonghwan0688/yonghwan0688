d07c7002f10a6ff03b93dcce440dbe0a646dea90
yonghwan <yonghwan0688@gmail.com>
2025-08-14 15:05:37 +0900

    0814_3

---
+++ study/0814/chapter12/nest-file-upload/dist/multer.options.d.ts
@@ -0,0 +1,3 @@
+export declare const multerOptions: {
+    storage: import("multer").StorageEngine;
+};
---
+++ study/0814/chapter12/nest-file-upload/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+  await app.listen(3000);
+}
+bootstrap();
